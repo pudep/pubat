b455082c4b31c1ee866312ad8d70dcd759370c0d
pudep <261843105+pudep@users.noreply.github.com>
2026-07-04 10:51:50 +0530

    same as previois commit

--- src/bin/render_text/initialize_viewport.rs
+++ src/bin/render_text/initialize_viewport.rs
@@ -1,6 +1,10 @@
-use ropey::Rope;
 use crate::app_state::ViewPort;
+use ropey::Rope;
 
-pub fn init(viewport: &mut ViewPort, rope: &Rope){
-  viewport.max_line = (rope.lines().len() - 1)as u32; 
+pub fn init(viewport: &mut ViewPort, rope: &Rope) {
+  if rope.lines().len() != 0 {
+    viewport.max_line = (rope.lines().len() - 1) as u32;
+  } else if rope.lines().len() == 0 {
+    viewport.max_line = (rope.lines().len()) as u32;
+  }
 }
